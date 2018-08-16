a57113f424af2d1cdf39879181939acd10e91fe4
chris <butchchr@gmail.com>
2018-08-15 23:50:39 -0400

    JS counter working

---
+++ gcCoffeeShop/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gcCoffeeShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+
+            HttpCookie userCookie = Request.Cookies["UserCookie"];
+            if (userCookie != null)
+            {
+                ViewBag.FirstName = userCookie.Values["FirstName"];
+                ViewBag.FavoriteCoffee = userCookie.Values["FavoriteCoffee"];
+            }
+
+            return View();
+        }
+    }
+}
