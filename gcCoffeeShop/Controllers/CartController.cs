55d271380166e08e94613b54d2da8bfea3c7f6c6
chris <butchchr@gmail.com>
2018-08-16 00:32:39 -0400

    working cart clear button, html cookie clear works too

---
+++ gcCoffeeShop/Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gcCoffeeShop.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            HttpCookie userCookie = Request.Cookies["UserCookie"];
+            if (userCookie != null)
+            {
+                ViewBag.FavoriteCoffee = userCookie.Values["FavoriteCoffee"];
+            }
+
+            HttpCookie quantityCookie = Request.Cookies["ProductQuantity"];
+            if (quantityCookie != null)
+            {
+                ViewBag.Quantity = quantityCookie.Value;
+            }
+
+            return View();
+        }
+    }
+}
